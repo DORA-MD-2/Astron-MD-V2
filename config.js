84f8e18bbcb0895d323268ee68c00b93e9561c95
Astron <154443828+nethmintha@users.noreply.github.com>
2025-01-25 20:25:19 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,6 +7,5 @@ function convertToBool(text, fault = "true") {
 }
 module.exports = {
   SESSION_ID: process.env.SESSION_ID || "vMFWzAJT#SUL477kcgy4h3BJtKepQA75oCp9ombC3hdNfZZNlN-w",
-  MONGODB: process.env.MONGODB || "mongodb://mongo:JADsENOSrlcxKcUfxZuwoIQuoPEQrsXj@roundhouse.proxy.rlwy.net:48219",
   OWNER_NUM: process.env.OWNER_NUM || "94784280074",
 };
