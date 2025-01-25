93646f947e84267b98bb21c8120b1c68db48acdf
Astron <154443828+nethmintha@users.noreply.github.com>
2025-01-25 20:41:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,4 +9,8 @@ module.exports = {
   SESSION_ID: process.env.SESSION_ID || "vMFWzAJT#SUL477kcgy4h3BJtKepQA75oCp9ombC3hdNfZZNlN-w",
   OWNER_NUM: process.env.OWNER_NUM || "94784280074",
   PREFIX: process.env.PREFIX || ".",
+  ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/m8xtvw.jpeg",
+  ALIVE_MSG: process.env.ALIVE_MSG || "Hello , I am alive now!!\n\n*🤡 Made by Nimsara 🤡*\n\n*watsapp channel link* - https://whatsapp.com/channel/0029Vb0bsRuFnSz4XAQ2yT0r",
+  AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+  MODE: process.env.MODE || "public",
 };
